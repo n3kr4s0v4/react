94e33ee0812f32b1108510c1d9ef83a7e8ef502c
n3kr4s0v4 <84867701+n3kr4s0v4@users.noreply.github.com>
2023-12-18 22:01:40 +0300

    fixed changes

--- week-2/src/SuperHero.jsx
+++ week-2/src/SuperHero.jsx
@@ -1,16 +1,14 @@
-import "./App.css";
-
 function SuperHero(props) {
-  return (
+    return (
     <div className="card">
-      <div className="card-body">
+    <div className="card-body">
         <img src={props.imgLink} alt={props.name} />
         <h4 className="card-title">Имя: {props.name}</h4>
         <p className="card-superpowers">Характеристики: {props.superpowers}</p>
         <p className="card-info">Подробная информация: {props.info}</p>
-      </div>
     </div>
-  );
+    </div>
+);
 }
 
 export default SuperHero;
