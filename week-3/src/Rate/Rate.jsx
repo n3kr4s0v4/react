634cd140bc7da095b894751458150e0fff1355fc
n3kr4s0v4 <84867701+n3kr4s0v4@users.noreply.github.com>
2023-12-21 19:28:52 +0300

    добавлено нажатие

--- week-3/src/Rate/Rate.jsx
+++ week-3/src/Rate/Rate.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import '../Rate/Rate.css'
 
-export default function Rate({title, background_title, background_price, price, speed, limite, active}) {
+export default function Rate({title, background_title, background_price, price, speed, limite}) {
     const backgroundTitle = {
         background: background_title
     };
     const backgroundPrice = {
         background: background_price
     };
+    const [active, setActive] = useState(false);
     return (
         <div className="rate">
             <div className='rate-card'>
-            <div className={active}>
+            <div onClick ={() => setActive(!active)} className={`rate-card-active ${active ? "transform" : ""}`}>
             <h4 className="rate-title" style={backgroundTitle}>{title}</h4>
             <p className="rate-price" style={backgroundPrice}>{price}</p>
             <p className="rate-speed">{speed}</p>
